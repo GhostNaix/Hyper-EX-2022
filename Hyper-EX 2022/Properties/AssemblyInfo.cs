7d67e52ffad7944ad176dc086d960cee5cc8db60
Naix <20989997+GhostNaix@users.noreply.github.com>
2024-03-09 05:32:55 +1100

    Update v17.9.2 First Commit
    [+] Added Support for Visual Studio 2022

---
+++ Hyper-EX 2022/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hyper-EX 2022")]
+[assembly: AssemblyDescription("A black Visual Studio theme inspired by Hyper Terminal")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hyper-EX 2022")]
+[assembly: AssemblyCopyright("Naix")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65233abb-f09a-4023-a94b-1ff690ad5dae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("17.0.0.0")]
+[assembly: AssemblyFileVersion("17.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-AU")]
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Hyper-EX 2022 A Hyper Terminal Theme - Visual Studio 2022
+A black Visual Studio theme inspired by Hyper Terminal
+
+![Theme Screenshot](/assets/sample.png)
+
+
+## Theme
+
+This theme is based on the default Visual Studio Dark theme with colours programmatically replaced colours from the Hyper colour scheme, and some manual changes. Please create an issue if you find any unreadable elements, or create a PR with a fix.
+
+## Installation
+
+Provided in [releases sections](https://github.com/GhostNaix/Hyper-EX-2022/releases/latest)
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Acknowledgments
+
+Base colours derived from [Hyper](https://github.com/zeit/hyper)
+Base dark theme and VSIX template created using [Visual Studio Color Theme Designer](https://marketplace.visualstudio.com/items?itemName=ms-madsk.ColorThemeDesigner)
+
